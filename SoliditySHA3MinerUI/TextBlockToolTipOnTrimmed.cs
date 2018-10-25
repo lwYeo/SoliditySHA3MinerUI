5cdfcb1c625aaf14fef71847e72692960c000ada
lwYeo <Lwyeo001@gmail.com>
2018-10-25 10:17:00 +0800

    Fixed very long Web3 RPC/Pool address issue in UI

---
+++ SoliditySHA3MinerUI/TextBlockToolTipOnTrimmed.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SoliditySHA3MinerUI
+{
+    public class TextBlockToolTipOnTrimmed : TextBlock
+    {
+        public TextBlockToolTipOnTrimmed()
+        {
+            SetBinding(ToolTipProperty, new Binding("Text") { Source = this });
+        }
+
+        protected override void OnToolTipOpening(ToolTipEventArgs e)
+        {
+            if (TextTrimming != TextTrimming.None)
+                e.Handled = !IsTextTrimmed();
+        }
+
+        private bool IsTextTrimmed()
+        {
+            var typeface = new Typeface(FontFamily, FontStyle, FontWeight, FontStretch);
+            var formattedText = new FormattedText(Text, CultureInfo.CurrentCulture, FlowDirection, typeface, FontSize, Foreground,
+                                                  VisualTreeHelper.GetDpi(this).PixelsPerDip);
+            return formattedText.Width > ActualWidth;
+        }
+    }
+}
